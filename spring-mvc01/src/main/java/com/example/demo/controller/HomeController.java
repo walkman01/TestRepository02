89c857e6106e4d79b1e5e84a6eb532ddaf0f4e9b
walkman01 <for.walkman01@gmail.com>
2021-11-16 14:28:59 +0900

    spring-mvc01

---
+++ spring-mvc01/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.model.User;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/form")
+    private String readForm(@ModelAttribute User user) {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    private String confirm(@ModelAttribute User user) {
+        return "confirm";
+    }
+}
