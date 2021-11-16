1be19a4ec8218f0a50d19e61669cd7677832c55a
walkman01 <94179338+walkman01@users.noreply.github.com>
2021-11-16 09:33:36 +0000

    for replit

--- spring-mvc01/src/main/java/com/example/demo/SpringMvc01Application.java
+++ spring-mvc01/src/main/java/com/example/demo/SpringMvc01Application.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringMvc01Application {
 
 	public static void main(String[] args) {
--- spring-mvc01/src/main/java/com/example/demo/model/User.java
+++ spring-mvc01/src/main/java/com/example/demo/model/User.java
@@ -8,23 +8,23 @@ public class User {
     private String email;
     private Integer age;
     
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getEmail() {
-//		return email;
-//	}
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//	public Integer getAge() {
-//		return age;
-//	}
-//	public void setAge(Integer age) {
-//		this.age = age;
-//	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
     
 }
