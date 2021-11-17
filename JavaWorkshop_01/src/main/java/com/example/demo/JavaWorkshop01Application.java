46955c35e3ac8fe8c6aad31a64872350bac92829
walkman01 <94179338+walkman01@users.noreply.github.com>
2021-11-17 05:48:44 +0000

    for replit working

--- JavaWorkshop_01/src/main/java/com/example/demo/JavaWorkshop01Application.java
+++ JavaWorkshop_01/src/main/java/com/example/demo/JavaWorkshop01Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaWorkshop01Application {
 
 	public static void main(String[] args) {
+    System.out.println("Hello world!");
 		SpringApplication.run(JavaWorkshop01Application.class, args);
+    System.out.println("End world!");
 	}
 
 }
