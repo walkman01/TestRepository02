bba86a40353ee8bee7c9a0e235adddb61394a744
walkman01 <94179338+walkman01@users.noreply.github.com>
2021-11-17 08:59:31 +0000

    for replit AllWorking

--- JavaWorkshop_01/src/main/java/com/example/demo/controller/HomeController.java
+++ JavaWorkshop_01/src/main/java/com/example/demo/controller/HomeController.java
@@ -9,12 +9,14 @@ import com.example.demo.model.User;
 @Controller
 public class HomeController {
 
-    @GetMapping("/form")
+//    @GetMapping("/form")
+    @GetMapping("form")
     private String readForm(@ModelAttribute User user) {
         return "form";
     }
 
-    @PostMapping("/form")
+//    @PostMapping("/form")
+    @PostMapping("form")
     private String confirm(@ModelAttribute User user) {
         return "confirm";
     }
